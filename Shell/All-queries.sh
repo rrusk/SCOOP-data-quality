75e707d76b611651697b345b43ca51bbd250edf4
Raymond Rusk <rusk.raymond@gmail.com>
2013-11-29 14:13:07 -0800

    Flush mysql output to log

--- Shell/All-queries.sh
+++ Shell/All-queries.sh
@@ -6,4 +6,4 @@ DB_NAME=`cat $SQL_DIR/db_name`
 PASSWD=`cat $SQL_DIR/passwd`
 SQL_REPO="$SQL_DIR/SCOOP-data-quality/PolypharmacySQLQueries"
 cd $SQL_REPO
-mysql -h 127.0.0.1 -P 3307 -u oscarb --database=$DB_NAME -vvv -t -AU --password=$PASSWD < All_queries.sql > $SQL_DIR/All-query-results.out 2>&1
+mysql -h 127.0.0.1 -P 3307 -u oscarb --database=$DB_NAME -n -vvv -t -AU --password=$PASSWD < All_queries.sql > $SQL_DIR/All-query-results.out 2>&1
