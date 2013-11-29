16bf55ffe81a776a1d85f2c0fe7aea119ba55e22
Raymond Rusk <rusk.raymond@gmail.com>
2013-11-29 14:07:51 -0800

    Script improvement and progess indication

--- PolypharmacySQLQueries/All_queries.sql
+++ PolypharmacySQLQueries/All_queries.sql
@@ -1,19 +1,58 @@
+-- SQL master script
+-- The SELECT statements provide progress information
+SELECT 'UBC_001_denominator.sql' AS ' ';
 source UBC_001_denominator.sql
+--
+SELECT 'UBC_001_numerator_meds_with_prn.sql' AS ' ';
 source UBC_001_numerator_meds_with_prn.sql
+--
+SELECT 'UBC_001_numerator_meds_without_prn.sql' AS ' ';
 source UBC_001_numerator_meds_without_prn.sql
+--
+SELECT 'UBC_001_numerator_rx_with_prn.sql' AS ' ';
 source UBC_001_numerator_rx_with_prn.sql
+--
+SELECT 'UBC_001_numerator_rx_without_prn.sql' AS ' ';
 source UBC_001_numerator_rx_without_prn.sql
+--
+SELECT 'UBC_002_denominator.sql' AS ' ';
 source UBC_002_denominator.sql
+--
+SELECT 'UBC_002_numerator_meds_with_prn.sql' AS ' ';
 source UBC_002_numerator_meds_with_prn.sql
+--
+SELECT 'UBC_002_numerator_meds_without_prn.sql' AS ' ';
 source UBC_002_numerator_meds_without_prn.sql
+--
+SELECT 'UBC_002_numerator_rx_with_prn.sql' AS ' ';
 source UBC_002_numerator_rx_with_prn.sql
+--
+SELECT 'UBC_002_numerator_rx_without_prn.sql' AS ' ';
 source UBC_002_numerator_rx_without_prn.sql
+--
+SELECT 'UBC_007_denominator.sql' AS ' ';
 source UBC_007_denominator.sql
+--
+SELECT 'UBC_007_numerator.sql' AS ' ';
 source UBC_007_numerator.sql
+--
+SELECT 'UBC_008_denominator.sql' AS ' ';
 source UBC_008_denominator.sql
+--
+SELECT 'UBC_008_numerator.sql' AS ' ';
 source UBC_008_numerator.sql
+--
+SELECT 'UBC_009_denominator.sql' AS ' ';
 source UBC_009_denominator.sql
+--
+SELECT 'UBC_009_numerator.sql' AS ' ';
 source UBC_009_numerator.sql
+--
+SELECT 'UBC_009_numerator_without_enc.sql' AS ' ';
 source UBC_009_numerator_without_enc.sql
+--
+SELECT 'UBC_010_denominator.sql' AS ' ';
 source UBC_010_denominator.sql
+--
+SELECT 'UBC_010_numerator.sql' AS ' ';
 source UBC_010_numerator.sql
--- Shell/All-queries.sh
+++ Shell/All-queries.sh
@@ -2,5 +2,8 @@
 # Assumes that the database name and password are dept in a secure directory
 set -e
 SQL_DIR="$HOME/mysql"
+DB_NAME=`cat $SQL_DIR/db_name`
+PASSWD=`cat $SQL_DIR/passwd`
 SQL_REPO="$SQL_DIR/SCOOP-data-quality/PolypharmacySQLQueries"
-mysql -h 127.0.0.1 -P 3307 -u oscarb --database=`"$SQL_DIR"/db_name` -vvv -t -AU --password=`"$SQL_DIR"/passwd` < "$SQL_REPO"/All_queries.sql > All-query-results.out 2>&1
+cd $SQL_REPO
+mysql -h 127.0.0.1 -P 3307 -u oscarb --database=$DB_NAME -vvv -t -AU --password=$PASSWD < All_queries.sql > $SQL_DIR/All-query-results.out 2>&1
