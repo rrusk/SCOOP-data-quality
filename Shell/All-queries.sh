ce136e7b7c66bdfab865c4d2ab7c0081cbc3dc47
Raymond Rusk <rusk.raymond@gmail.com>
2013-11-29 13:35:20 -0800

    Set to stop on errors

--- Shell/All-queries.sh
+++ Shell/All-queries.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Assumes that the database name and password are dept in a secure directory
+set -e
 SQL_DIR="$HOME/mysql"
 SQL_REPO="$SQL_DIR/SCOOP-data-quality/PolypharmacySQLQueries"
-mysql -h 127.0.0.1 -P 3307 -u oscarb --database=`"$SQL_DIR"/db_name` -vvv -t -AU --password=`"$SQL_DIR"/passwd` < "$SQL_REPO"/All-queries.sql > All-query-results.out 2>&1
-QP
+mysql -h 127.0.0.1 -P 3307 -u oscarb --database=`"$SQL_DIR"/db_name` -vvv -t -AU --password=`"$SQL_DIR"/passwd` < "$SQL_REPO"/All_queries.sql > All-query-results.out 2>&1
