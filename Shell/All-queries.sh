ff3b9158ee8a8584e54fee2cfbcf3b7bafaef292
Raymond Rusk <rusk.raymond@gmail.com>
2013-12-09 11:56:06 -0800

    Fix typos

--- Shell/All-queries.sh
+++ Shell/All-queries.sh
@@ -2,11 +2,11 @@
 # Assumes that the database db_user, db_passwd, db_name and db_port are kept in a secure directory ~/mysql
 set -e
 DATETIME=`date +"%Y%m%d%H%M"`
-SQL_DIR="$HOME/mysql/db_config"
-DB_USER=`cat $SQL_DIR/db_user`
-DB_NAME=`cat $SQL_DIR/db_name`
-DB_PASSWD=`cat $SQL_DIR/db_passwd`
-DB_PORT=`cat $SQL_DIR/db_port`
+SQL_DIR="$HOME/mysql"
+DB_USER=`cat $SQL_DIR/db_config/db_user`
+DB_NAME=`cat $SQL_DIR/db_config/db_name`
+DB_PASSWD=`cat $SQL_DIR/db_config/db_passwd`
+DB_PORT=`cat $SQL_DIR/db_config/db_port`
 SQL_REPO="$SQL_DIR/SCOOP-data-quality/PolypharmacySQLQueries"
 cd $SQL_REPO
 mysql -h 127.0.0.1 -P $DB_PORT -u $DB_USER --database=$DB_NAME -n -vvv -t -AU --password=$DB_PASSWD \
