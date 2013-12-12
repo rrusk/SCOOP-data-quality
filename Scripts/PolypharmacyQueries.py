4e07dd7861e6f6775993bf7963feb63ad5aa9f1c
Raymond Rusk <rusk.raymond@gmail.com>
2013-12-12 12:19:39 -0800

    First working version for UBC-001 and UBC-002 queries

--- Scripts/PolypharmacyQueries.py
+++ Scripts/PolypharmacyQueries.py
@@ -13,20 +13,73 @@ import time
 con = None
 f = None
 
-query1 = ("SELECT COUNT(DISTINCT d.demographic_no) AS 'Count' "
-          "FROM demographic AS d "
-          "INNER JOIN drugs AS dr ON d.demographic_no = dr.demographic_no "
-          "WHERE d.patient_status = 'AC' AND "
-          "CONCAT_WS( '-',d.year_of_birth,d.month_of_birth,d.date_of_birth ) < "
-          "DATE_SUB( NOW(), INTERVAL 64 YEAR )")
-
-query2 = ("SELECT dr.demographic_no FROM drugs AS dr "
+polypharmacy = ("SELECT dr.demographic_no FROM drugs AS dr "
           "WHERE dr.archived = 0 AND dr.regional_identifier IS NOT NULL AND "
             "dr.regional_identifier != '' AND dr.ATC IS NOT NULL AND "
             "dr.ATC != '' AND rx_date <= NOW() AND "
             "(DATE_ADD(dr.rx_date, INTERVAL(DATEDIFF(dr.end_date,dr.rx_date)*1.2) DAY)) >= NOW() "
           "GROUP BY dr.demographic_no "
-          "HAVING COUNT(DISTINCT dr.regional_identifier) >= 5")
+          "HAVING COUNT(DISTINCT dr.regional_identifier) >= ")
+
+polypharmacy_nonunique = ("SELECT dr.demographic_no FROM drugs AS dr "
+          "WHERE dr.archived = 0 AND dr.regional_identifier IS NOT NULL AND "
+            "dr.regional_identifier != '' AND dr.ATC IS NOT NULL AND "
+            "dr.ATC != '' AND rx_date <= NOW() AND "
+            "(DATE_ADD(dr.rx_date, INTERVAL(DATEDIFF(dr.end_date,dr.rx_date)*1.2) DAY)) >= NOW() "
+          "GROUP BY dr.demographic_no "
+          "HAVING COUNT(dr.regional_identifier) >= ")
+
+polypharmacy_without_prn = ("SELECT dr.demographic_no FROM drugs AS dr "
+          "WHERE dr.archived = 0 AND dr.prn IN (0) AND dr.regional_identifier IS NOT NULL AND "
+            "dr.regional_identifier != '' AND dr.ATC IS NOT NULL AND "
+            "dr.ATC != '' AND rx_date <= NOW() AND "
+            "(DATE_ADD(dr.rx_date, INTERVAL(DATEDIFF(dr.end_date,dr.rx_date)*1.2) DAY)) >= NOW() "
+          "GROUP BY dr.demographic_no "
+          "HAVING COUNT(DISTINCT dr.regional_identifier) >= ")
+
+
+polypharmacy_nonunique_without_prn = ("SELECT dr.demographic_no FROM drugs AS dr "
+          "WHERE dr.archived = 0 AND dr.prn IN (0) AND dr.regional_identifier IS NOT NULL AND "
+            "dr.regional_identifier != '' AND dr.ATC IS NOT NULL AND "
+            "dr.ATC != '' AND rx_date <= NOW() AND "
+            "(DATE_ADD(dr.rx_date, INTERVAL(DATEDIFF(dr.end_date,dr.rx_date)*1.2) DAY)) >= NOW() "
+          "GROUP BY dr.demographic_no "
+          "HAVING COUNT(dr.regional_identifier) >= ")
+
+
+target_group = ("SELECT d.demographic_no FROM demographic AS d WHERE d.patient_status = 'AC' AND "
+          "CONCAT_WS( '-',d.year_of_birth,d.month_of_birth,d.date_of_birth ) < "
+          "DATE_SUB( NOW(), INTERVAL 64 YEAR ) AND d.demographic_no = ")
+
+without_prn = "without_prn"
+with_prn = "with_prn"
+unique = 'unique'
+not_unique = 'not_unique'
+
+def print_result(cur, num_drugs, prn, unq):
+    if prn is with_prn:
+        if unq is unique:
+            cur.execute(polypharmacy + str(num_drugs))
+        else:
+            cur.execute(polypharmacy_nonunique + str(num_drugs))
+    else:
+        if unq is unique:
+            cur.execute(polypharmacy_without_prn + str(num_drugs))
+        else:
+            cur.execute(polypharmacy_nonunique_without_prn + str(num_drugs))
+    rows = cur.fetchall()
+
+    demoids = []
+    for row in rows:
+        demoids.append(str(row[0]))
+
+    polycount = []
+    for demoid in demoids:
+        cur.execute(target_group + demoid)
+        rows = cur.fetchall()
+        for row in rows:
+            polycount.append(row)
+    print len(polycount),
 
 try:
     from os.path import expanduser
@@ -50,26 +103,27 @@ try:
     con = mdb.connect(host='127.0.0.1', port=db_port, user=db_user, passwd=db_passwd, db=db_name)
 
     cur = con.cursor()
-    cur.execute(query1)
 
-    rows = cur.fetchall()
-
-    cur.execute(query2)
-    rows2 = cur.fetchall()
-
-    for row in rows:
-        print "%s" % row
-
-    demoids = []
-    for row2 in rows2:
-        demoids.append(str(row2[0]))
-    instring = ''.join(demoids)
+    print "5 meds, with prn, nonunique /"
+    print "5 meds, without prn, nonunique /"
+    print "5 meds, with prn, unique /"
+    print "5 meds, without prn, unique /"
+    print "10 meds, with prn, nonunique /"
+    print "10 meds, without prn, nonunique /"
+    print "10 meds, with prn, unique /"
+    print "10 meds, without prn, unique"
 
     start_time = time.time()
-    cur.execute(query1 + " AND d.demographic_no IN (" + query2 +")")
-    rows3 = cur.fetchone()
-    print "%s" % rows3
-    print time.time() - start_time, "seconds"
+    print_result(cur, 5, with_prn, not_unique)
+    print_result(cur, 5, without_prn, not_unique)
+    print_result(cur, 5, with_prn, unique)
+    print_result(cur, 5, without_prn, unique)
+    print_result(cur, 10, with_prn, not_unique)
+    print_result(cur, 10, without_prn, not_unique)
+    print_result(cur, 10, with_prn, unique)
+    print_result(cur, 10, without_prn, unique)
+    print
+    print time.time() -  start_time, "seconds"
 
 except mdb.Error, e:
 
