31f1fa44eabeae9e30709ae27ee7ae7b7ee5e195
Raymond Rusk <rusk.raymond@gmail.com>
2014-01-08 09:52:24 -0800

    added additional queries

--- Scripts/duplicates_query.py
+++ Scripts/duplicates_query.py
@@ -4,7 +4,7 @@
 # ~/mysql/db_config.  Change if needed.  Variables are initialized
 # this way for compatibility with prior Bash script.
 #
-__author__ = 'rrusk'
+__author__ = 'drusk and rrusk'
 
 import collections
 import hashlib
@@ -13,6 +13,16 @@ import sys
 
 import MySQLdb as mdb
 
+query1 = """SELECT demographic_no, hin, last_name, first_name, year_of_birth, month_of_birth,
+                          date_of_birth, sex, patient_status
+                   FROM demographic"""
+
+query2 = """SELECT demographic_no, last_name, first_name, year_of_birth, month_of_birth,
+                          date_of_birth, patient_status
+                   FROM demographic"""
+
+query3 = """SELECT demographic_no, hin, patient_status
+                   FROM demographic"""
 
 def read_config(filename):
     home = os.path.expanduser("~")
@@ -32,13 +42,11 @@ def get_connection():
     return mdb.connect(host='127.0.0.1', port=db_port, user=db_user, passwd=db_passwd, db=db_name)
 
 
-def get_patients(con):
+def get_patients(con, query):
     patients = collections.defaultdict(list)
 
     cur = con.cursor()
-    cur.execute("""SELECT demographic_no, hin, last_name, first_name, year_of_birth, month_of_birth,
-                          date_of_birth, sex, patient_status
-                   FROM demographic""")
+    cur.execute(query)
 
     for record in cur.fetchall():
         hasher = hashlib.sha224()
@@ -61,7 +69,9 @@ def main():
     con = get_connection()
 
     try:
-        patients = get_patients(con)
+        patients = get_patients(con, query1)
+        patients2 = get_patients(con, query2)
+        patients3 = get_patients(con, query3)
     except mdb.Error as e:
         print "Error %d: %s" % (e.args[0], e.args[1])
         sys.exit(1)
@@ -69,13 +79,30 @@ def main():
         if con:
             con.close()
 
-    print "Total hashkeys: %d" % len(patients)
+    print "Total hashkeys based on hin, last name, first name, birth date and gender: %d" % len(patients)
 
     for hashkey, patient_info in patients.iteritems():
         if len(patient_info) > 1:
-            print "Collision for hashkey: %s." % hashkey
+            print "Collision for hin, last name, first name, birth date and gender: %s." % hashkey
             for i, patient in enumerate(patient_info):
-                print " %d: Demographic id: %s, Patient status: %s" % (i, patient[0], patient[1])
+                print " %d: demographic_no: %s, patient_status: %s" % (i, patient[0], patient[1])
+
+
+    print "\n\nTotal hashkeys based on last name, first name and birth date: %d" % len(patients2)
+
+    for hashkey, patient_info in patients2.iteritems():
+        if len(patient_info) > 1:
+            print "Collision for last name, first name and birth date: %s." % hashkey
+            for i, patient in enumerate(patient_info):
+                print " %d: demographic_no: %s, patient_status: %s" % (i, patient[0], patient[1])
+
+    print "\n\nTotal hashkeys based on hin: %d" % len(patients3)
+
+    for hashkey, patient_info in patients3.iteritems():
+        if len(patient_info) > 1:
+            print "Collision for hin: %s." % hashkey
+            for i, patient in enumerate(patient_info):
+                print " %d: demographic_no: %s, patient_status: %s" % (i, patient[0], patient[1])
 
 
 if __name__ == "__main__":
