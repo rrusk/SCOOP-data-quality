d1adae6d50e2d62ce7c540910eaed6c7f0bcafdf
Raymond Rusk <rusk.raymond@gmail.com>
2015-05-27 10:00:11 -0700

    Add provider list

---
+++ Javascript_Queries/setStoppDQProviders.js
@@ -0,0 +1,34 @@
+/**
+ * Created by rrusk on 08/04/15.
+ */
+function setStoppDQProviders() {
+    return [
+        'wAFM/FKbT1fjLL4NLP+WvZICcgX8Sc9mTVdISg==',
+        'UrodGkttC9YETfPaG2FbCq/UEexM1JyNDExLjg==',
+        'y+OGm1UaYal9ydpIIE26ddMcmflVDZd2VbohtQ==',
+        'IMuQgGy0JbqOA9aceglf5FzKV0u2POhcSKWb1w==',
+        'mr/fY50QkgVv3OLNuDoFJUBNICCNGxqsz6mP3A==',
+        'dSF49leCR614gXaTjZ4oTy9MpuZ83AHjS6ZiAA==',
+        '6hciDv3K3d/uhlFTJ8rXj3FTOkgg30i24ufT1w==',
+        'pCRvWxkze0gk6kTrXrrN9hQnc0iiNltPBnRDOQ==',
+        'eXQiTjUPBHNDN9v0H7fn7N9HlgGZv3KyxMW7JQ==',
+        'ggNh+qMuj695LPFnRKJdHpmuwptn8m25OB3m7Q==',
+        'Snn8YC7WeZQSL1/3FbPaTQZtAjtkO4oXHMNUsA==',
+        'BgOLx42PP7kT17ZNwQhVWWMjR9WJYyoJBEx7dA==',
+        'B63m7xoU83mwtz2RIl1aN0HE3PPAOsdDiGNhPw==',
+        'kxDpGMv+o1ZFJEXV/9LUTW1tA7uVv3c/fhT13Q==',
+        'R12lDOzkIzzsRIBEdO1JDze9Ud/HauUvRRCl9Q==',
+        'kKWDni0CYlV1ugXRoqyPC9pSh8yUGkAMmmxBTQ==',
+        'aDNVDMdceJfpcheqbCMaQxIeQ8iFkbQYxhdMFA==',
+        'qjfwYUf9XoHjPNjai1h+lF38+kld4jQSaE+U9A==',
+        'zcpixI39t6YdDvNMtGPZJR96gYfUtQBFcuhP/g==',
+        'X++diHXah4A4UALyloSHwIe2Z4mCo3Bko4R6gA==',
+        '1F/Md0SM07z0uuUU0Wnod93SYWBEQQrvUae4PQ==',
+        '1rS/7f/aSZOooZm8HB8QPubP4BhmoDN8PmF+mQ==',
+        '7pIaS7sqYF5DMz59H7+9l8wbDri1Fyt4ATNSPA==',
+        'fFMbjmUs+eRL7sPw2M7Hl2+3YjEOayRjLyYKlQ==',
+        '1RhcfZAi+QS9Sn5iqEi6g2JQtgCiQo87z9xQvA==',
+        'Wnfs1l/7yy0lVD1IpAom3IoJiOgquWJdrBT/iQ==',
+        'ckZxzBgJRg2vCmHeenVCtesgnz7knloak0+88Q=='
+    ];
+}
